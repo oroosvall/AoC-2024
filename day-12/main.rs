4240f3aafd6e62d11e578af1a9c828c3de965637
Oscar Roosvall <10653734+oroosvall@users.noreply.github.com>
2024-12-15 12:52:32 +0100

    AoC-day12 - part 2

--- day-12/main.rs
+++ day-12/main.rs
@@ -12,18 +12,6 @@ fn read_to_vec<R: Read>(io: R) -> Result<Vec<String>, Error> {
     Ok(lines)
 }
 
-// fn is_different(v: &Vec<String>, p: (i32, i32), c: char) -> bool {
-//     if p.1 >= 0 && p.1 < v.len() as i32 {
-//         if p.0 >= 0 && p.0 < v[0].len() as i32 {
-//             let cc = v[p.1 as usize].chars().nth(p.0 as usize).unwrap();
-//             if cc != c {
-//                 return true;
-//             }
-//         }
-//     }
-//     false
-// }
-
 fn to_pos(idx: i32, s: i32) -> (i32, i32) {
     (idx % s, idx / s)
 }
@@ -81,8 +69,6 @@ fn get_plant_region(
         idx = next_idx.pop();
     }
 
-    // println!("{:?}", visited);
-
     for i in visited {
         s_bytes[i] = b' ';
     }
@@ -104,8 +90,6 @@ fn part_1(v: &Vec<String>) -> u64 {
         .map(|plant| {
             let mut sum = 0;
             while let Some(p) = get_plant_region(&mut m, s, plant) {
-                // println!("{:?}", m);
-                // println!("{}: {} * {} -> {}", plant, p.0, p.1, p.0 * p.1);
                 sum += p.0 * p.1;
             }
             sum as u64
@@ -113,66 +97,125 @@ fn part_1(v: &Vec<String>) -> u64 {
         .sum::<u64>()
 }
 
-// fn part_1(v: &Vec<String>) -> u64 {
-//     let m: Vec<Vec<(char, i32)>> = v
-//         .into_iter()
-//         .enumerate()
-//         .map(|(y, l)| {
-//             l.chars()
-//                 .enumerate()
-//                 .map(|(x, ch)| {
-//                     let mut perimiter = 0;
-//                     if y == 0 || y == v.len() - 1 {
-//                         // top/bottm is guaranteed to have 1 perimiter block
-//                         perimiter += 1;
-//                     }
-//                     if x == 0 || x == l.len() - 1 {
-//                         // sides a is guaranteed to have 1 perimiter block
-//                         perimiter += 1;
-//                     }
+type TestPos = ((i32, i32), (i32, i32), (i32, i32));
 
-//                     let px = x as i32;
-//                     let py = y as i32;
+fn test_corner(m: &mut [u8], s: i32, ch: char, tp: TestPos) -> bool {
+    let p1 = to_idx(tp.0, s) as usize;
+    let c = to_idx(tp.1, s) as usize;
+    let p2 = to_idx(tp.2, s) as usize;
 
-//                     let test_positions = [(px - 1, py), (px + 1, py), (px, py - 1), (px, py + 1)];
+    let cb = ch as u8;
 
-//                     for tp in test_positions {
-//                         if is_different(&v, tp, ch) {
-//                             perimiter += 1;
-//                         }
-//                     }
+    if m[c] != cb {
+        if m[p1] != cb && m[p2] != cb {
+            return true;
+        } else if m[p1] == cb && m[p2] == cb {
+            return true;
+        }
+    } else {
+        if m[p1] != cb && m[p2] != cb {
+            return true;
+        }
+    }
+    false
+}
 
-//                     (ch, perimiter)
-//                 })
-//                 .collect()
-//         })
-//         .collect();
+fn get_plant_edges(m: &mut String, s: (usize, usize), ch: char) -> std::option::Option<(i32, i32)> {
+    let mut idx = m.find(ch);
 
-//     let mut nv: Vec<(char, i32)> = vec![];
-//     for l in m {
-//         nv.extend(&l);
-//         println!("{:?}", l);
-//     }
+    let mut visited = vec![];
+    let mut next_idx = vec![];
 
-//     "ABCDEFGHIJKLMNOPQRSTUVXYZW"
-//         .chars()
-//         .map(|c| {
-//             let p: Vec<&i32> = nv
-//                 .iter()
-//                 .filter(|(ch, _)| *ch == c)
-//                 .map(|(_, val)| val)
-//                 .collect();
-//             let l = p.len() as u64 * p.into_iter().sum::<i32>() as u64;
-//             if l != 0 {
-//                 println!("{} {}", c, l);
-//             }
-//             l
-//         })
-//         .sum()
-// }
+    let mut chs = 0;
+    let mut corners = 0;
 
-fn part_2(_v: &Vec<String>) -> u64 {
-    0
+    let s_bytes: &mut [u8] = unsafe { m.as_bytes_mut() };
+
+    while let Some(i) = idx {
+        // println!("{}", i);
+        visited.push(i);
+        chs += 1;
+
+        let (px, py) = to_pos(i as i32, s.0 as i32);
+
+        if test_corner(
+            s_bytes,
+            s.0 as i32,
+            ch,
+            ((px - 1, py), (px - 1, py - 1), (px, py - 1)),
+        ) {
+            corners += 1;
+        }
+        if test_corner(
+            s_bytes,
+            s.0 as i32,
+            ch,
+            ((px + 1, py), (px + 1, py - 1), (px, py - 1)),
+        ) {
+            corners += 1;
+        }
+        if test_corner(
+            s_bytes,
+            s.0 as i32,
+            ch,
+            ((px - 1, py), (px - 1, py + 1), (px, py + 1)),
+        ) {
+            corners += 1;
+        }
+        if test_corner(
+            s_bytes,
+            s.0 as i32,
+            ch,
+            ((px + 1, py), (px + 1, py + 1), (px, py + 1)),
+        ) {
+            corners += 1;
+        }
+
+        let test_positions = [(px - 1, py), (px + 1, py), (px, py - 1), (px, py + 1)];
+
+        for tp in test_positions {
+            let tp_idx = to_idx(tp, s.0 as i32) as usize;
+            let t_ch = s_bytes[tp_idx] as char;
+            if !visited.contains(&tp_idx) && !next_idx.contains(&tp_idx) && t_ch == ch {
+                next_idx.push(tp_idx);
+            }
+        }
+
+        idx = next_idx.pop();
+    }
+
+    for i in visited {
+        s_bytes[i] = b' ';
+    }
+
+    if chs != 0 {
+        return Some((chs, corners));
+    }
+    None
+}
+
+fn part_2(v: &Vec<String>) -> u64 {
+    let mut vv: Vec<String> = v.into_iter().map(|l| " ".to_string() + l + " ").collect();
+    vv.push(" ".repeat(vv[0].len()));
+    vv.insert(0, " ".repeat(vv[0].len()));
+
+    let s = (vv[0].len(), vv.len());
+    let mut m: String = vv
+        .into_iter()
+        .fold("".to_string(), |cur: String, nxt: String| cur + &nxt);
+
+    "ABCDEFGHIJKLMNOPQRSTUVXYZW"
+        .chars()
+        .map(|plant| {
+            let mut sum = 0;
+            while let Some(p) = get_plant_edges(&mut m, s, plant) {
+                // println!("{:?}", m);
+                // println!("{}: {} * {} -> {}", plant, p.0, p.1, p.0 * p.1);
+                sum += p.0 * p.1;
+            }
+            sum as u64
+        })
+        .sum::<u64>()
 }
 
 fn main() -> Result<(), Error> {
